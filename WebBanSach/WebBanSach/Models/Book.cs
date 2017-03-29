8eec01b2246495aacf42c6b4f73b6e587558443d
doanthanhhuong <huongthanhlan@gmail.com>
2017-03-30 00:55:31 +0700

    XAY  DUNG CO BAN  LAYOUT

---
+++ WebBanSach/WebBanSach/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanSach.Models
+{
+    public class Book
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string autherName { get; set; }
+        public string cost { get; set; }
+        public string detail { get; set; }
+        //public string picture { get; set; }
+        //public int numberPage { get; set; }
+        //public int number { get; set; }
+        //public DateTime PublicationDate { get; set; }
+        //public string Publisher { get; set; }
+        //public int BestSellerRank { get; set; }
+       
+
+
+
+    }
+}
